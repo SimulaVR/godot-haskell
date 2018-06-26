1b4a41f11a65b2344fdd4d322d1ce221999b5d3f
David Kraeutmann <kane@kane.cx>
2018-06-27 01:21:50 +0200

    make it compile

--- src/Godot/Nativescript.hs
+++ src/Godot/Nativescript.hs
@@ -1,6 +1,10 @@
 {-# LANGUAGE RecursiveDo #-}
 module Godot.Nativescript where
 
+import Data.Typeable
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+
 import Foreign
 import Foreign.C
 
@@ -40,15 +44,23 @@ registerMethod :: Typeable a
                -> String -- ^ class name
                -> String -- ^ method name
                -> GodotMethodRpcMode
-               -> (GodotObject -> a -> Vector GodotVariant -> IO GodotVariant)
+               -> (GodotObject -> a -> Vector GodotVariant -> IO GodotVariant) -- ^ the godot variants are destroyed at the end of the call
                -> IO ()
 registerMethod pHandle clsName mtdName rpc method  = do
-  methodFun <- mkInstanceMethodFun $ \outPtr ins _ objPtr numArgs argsPtr = do
+  methodFun <- mkInstanceMethodFunPtr $ \outPtr ins _ objPtr numArgs argsPtr -> do
     obj <- deRefStablePtr $ castPtrToStablePtr objPtr
-    undefined
---    V.generateM numArgs $
---      \idx -> GodotVariant (argsPtr `plusPtr` (opaqueSizeOf @GodotVariant * idx))
+    args <- V.generateM (fromIntegral numArgs) $
+      \idx -> GodotVariant <$> newForeignPtr_ (argsPtr `plusPtr` (opaqueSizeOf @GodotVariant * idx))
     res <- method ins obj args
+    withGodotVariant res $ \resPtr -> copyBytes outPtr resPtr (opaqueSizeOf @GodotVariant)
+    return outPtr
+    
+  rec
+    methodFreeFun <- mkInstanceFreeFunPtr $
+      \_ -> freeHaskellFunPtr methodFun >> freeHaskellFunPtr methodFreeFun
 
+  let methodObj = GodotInstanceMethod methodFun nullPtr methodFreeFun
+  
+  withCString clsName $ \clsNamePtr -> withCString mtdName $
+    \mtdNamePtr -> godot_nativescript_register_method pHandle clsNamePtr mtdNamePtr (GodotMethodAttributes rpc) methodObj
 
--- void GDAPI godot_nativescript_register_method(void *p_gdnative_handle, const char *p_name, const char *p_function_name, godot_method_attributes p_attr, godot_instance_method p_method);
