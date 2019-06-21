88f27a2f5641c69fd1f8cac85326c9db803d87bf
Ludvig Böklin <ludvig.boklin@protonmail.com>
2019-06-21 14:39:49 +0200

    Make newtype constructors opaque and refactor demo
    Other changes:
    * Style fixes in both Nativescript.hs and demo
    * Refactor parts of demo to stay closer to its GDScript equivalent
    
    Fix memory leak, fix and refactor demo, misc changes

--- demo/src/Lib.hs
+++ demo/src/Lib.hs
@@ -1,35 +1,44 @@
-{-# LANGUAGE TemplateHaskell, MultiParamTypeClasses, OverloadedStrings #-}
-{-# LANGUAGE FlexibleContexts, ScopedTypeVariables, TypeApplications, TypeFamilies #-}
+{-# LANGUAGE
+  FlexibleContexts,
+  MultiParamTypeClasses,
+  OverloadedStrings,
+  TemplateHaskell,
+  TypeApplications,
+  TypeFamilies #-}
 module Lib
   ( exports
   )
 where
 
-import           Control.Monad
-import           Godot
-import           Godot.Gdnative
-import           Linear.V2
-import           Linear.Metric(norm,normalize)
-import           Linear.Vector
-import           Util
 import           Control.Lens
-import           System.Random
-import           Foreign.C.Types
-import qualified Data.Text as T
-import           Godot.Core.Node as G
-import           Godot.Core.Object as G
-import           Godot.Core.CanvasItem as G
-import           Godot.Core.Timer as GT
-import           Godot.Core.Node2D as G
-import           Godot.Core.AnimatedSprite as G
-import           Godot.Core.Label as G
-import           Godot.Core.PathFollow2D as GP
-import           Godot.Core.PackedScene as GS
-import           Godot.Core.RigidBody2D as G
-import           Godot.Core.CollisionShape2D as G
-import           Godot.Core.Input as G
+import           Control.Arrow --((>>>))
+import           Control.Monad
+import           Control.Monad.Extra (whenJust, whenJustM)
 import           Data.Coerce
-import           Godot.Core.Reference
+import qualified Data.Map.Strict as M
+import qualified Data.Text as T
+import           Data.Typeable
+import           Foreign.C.Types
+import           Godot
+import           Godot.Core.AnimatedSprite as AnimatedSprite
+import           Godot.Core.Area2D as Area2D
+import           Godot.Core.CanvasItem as CanvasItem
+import           Godot.Core.CollisionShape2D as CollisionShape2D
+import           Godot.Core.Input as Input
+import           Godot.Core.Label as Label
+import           Godot.Core.Node as Node
+import           Godot.Core.Node2D as Node2D
+import           Godot.Core.Object as Object
+import           Godot.Core.PackedScene as PackedScene
+import           Godot.Core.PathFollow2D as PathFollow2D
+import           Godot.Core.Reference as Reference
+import           Godot.Core.RigidBody2D as RigidBody2D
+import           Godot.Core.Timer as Timer
+import           Godot.Gdnative
+import           Linear.Metric (normalize)
+import           Linear.V2
+import           Linear.Vector
+import           System.Random
 
 exports :: GdnativeHandle -> IO ()
 exports desc = do
@@ -38,70 +47,93 @@ exports desc = do
   registerClass $ RegClass desc $ classInit @Mob
   registerClass $ RegClass desc $ classInit @HUD
 
-data Main = Main { _mainBase :: Node, _mainScore :: MVar Int, _mainMobScene :: MVar PackedScene }
+
+--- Main
+
+
+data Main = Main
+  { _mainBase :: Node
+  , _mainScore :: MVar Int
+  , _mainMobScene :: MVar PackedScene
+  }
 
 instance HasBaseClass Main where
   type BaseClass Main = Node
   super = _mainBase
 instance NativeScript Main where
-  classInit base = Main base <$> newMVar 0 <*> newMVar (PackedScene emptyObject)
-  classMethods = [method0 "game_over" game_over
-                 ,method0 "new_game" new_game
-                 ,method0 "_on_StartTimer_timeout" on_StartTimer_timeout
-                 ,method0 "_on_ScoreTimer_timeout" on_ScoreTimer_timeout
-                 ,method0 "_on_MobTimer_timeout" on_MobTimer_timeout
-                 ]
-  classProperties = [
-    createMVarProperty "PackedScene" _mainMobScene (PackedScene emptyObject)
+  classInit base = Main base <$> newMVar 0 <*> newEmptyMVar
+  classMethods =
+    [ method0 "_on_MobTimer_timeout" on_MobTimer_timeout
+    , method0 "_on_StartTimer_timeout" on_StartTimer_timeout
+    , method0 "_on_ScoreTimer_timeout" on_ScoreTimer_timeout
+    , method0 "game_over" game_over
+    , method0 "new_game" new_game
+    ]
+  classProperties =
+    [ createMVarProperty "PackedScene" _mainMobScene (Left VariantTypeObject)
     ]
 
 game_over :: Main -> IO ()
 game_over self = do
-  GT.stop =<< getNode @Timer self "ScoreTimer"
-  GT.stop =<< getNode @Timer self "MobTimer"
+  getNode @Timer self "ScoreTimer" >>= Timer.stop
+  getNode @Timer self "MobTimer" >>= Timer.stop
   _ <- show_game_over =<< getNodeNativeScript @HUD self "HUD"
   pure ()
 
+new_game :: Main -> IO ()
 new_game self = do
-  _ <- swapMVar (_mainScore self) 0
-  position <- get_position =<< getNode @Position2D self "StartPosition"
-  join $ call <$> getNodeNativeScript @Player self "Player" <*> toLowLevel "start" <*> pure [toVariant position]
-  join $ start <$> getNode @Timer self "StartTimer" <*> pure (-1)
+  _        <- swapMVar (_mainScore self) 0
+  position <- getNode @Position2D self "StartPosition" >>= get_position
+  join
+    $   call
+    <$> getNodeNativeScript @Player self "Player"
+    <*> toLowLevel "start"
+    <*> pure [toVariant position]
+  getNode @Timer self "StartTimer" >>= start `flip` (-1)
   hud <- getNodeNativeScript @HUD self "HUD"
   update_score hud 0
-  show_message hud =<< toLowLevel ("Get Ready!" :: Text)
-  pure ()
+  show_message hud =<< toLowLevel "Get Ready!"
 
+on_StartTimer_timeout :: Main -> IO ()
 on_StartTimer_timeout self = do
-  mt <- getNode @Timer self "MobTimer"
-  start mt (-1)
-  st <- getNode @Timer self "ScoreTimer"
-  start st (-1)
+  getNode @Timer self "MobTimer" >>= start `flip` (-1)
+  getNode @Timer self "ScoreTimer" >>= start `flip` (-1)
 
+on_ScoreTimer_timeout :: Main -> IO ()
 on_ScoreTimer_timeout self = do
   modifyMVar_ (_mainScore self) (pure . (+) 1)
-  join $ update_score <$> (getNodeNativeScript @HUD self "HUD") <*> readMVar (_mainScore self)
+  join $ update_score <$> getNodeNativeScript @HUD self "HUD" <*> readMVar
+    (_mainScore self)
 
-on_MobTimer_timeout self = do
-  mobSpawnLocation <- getNode @PathFollow2D self "MobPath/MobSpawnLocation"
-  -- Set the mob's position to a random location.
-  i :: Float <- randomIO
-  GP.set_v_offset mobSpawnLocation 0
-  GP.set_unit_offset mobSpawnLocation i
-  mob <- readMVar $ _mainMobScene self
-  imob <- GS.instance' mob 0
-  (Just (mobInstance :: RigidBody2D)) <- tryCast imob
-  add_child self (upcast mobInstance) False
-  -- Set the mob's direction perpendicular to the path direction.
-  direction <- (+ (pi / 2)) <$> get_rotation mobSpawnLocation
-  set_position mobInstance =<< get_position mobSpawnLocation
-  direction' <- (+ direction) <$> randomRIO (- pi / 4, pi / 4)
-  set_rotation mobInstance direction'
-  set_linear_velocity mobInstance
-    =<< (\v -> godot_vector2_rotated v (CFloat direction'))
-    =<< toLowLevel
-    =<< liftM2 V2 (randomRIO (150 :: Float, 250)) (pure 0)
-  pure ()
+on_MobTimer_timeout :: Main -> IO ()
+on_MobTimer_timeout self@(Main _ _ mobScene) = do
+  -- Choose a random location on Path2D.
+  mobSpawnLoc <- getNode @PathFollow2D self "MobPath/MobSpawnLocation"
+  PathFollow2D.set_offset mobSpawnLoc =<< fromInteger <$> randomIO
+  -- Create a Mob instance and add it to the scene.
+  mob <-
+    readMVar mobScene
+    >>= (PackedScene.instance' `flip` 0)
+    >>= (asNativeScript . upcast)
+    >>= maybe (error "Couldn't cast mob to NativeScript") pure
+  add_child self (upcast mob) False
+  -- -- Set the mob's direction perpendicular to the path direction.
+  direction <- get_rotation mobSpawnLoc <&> (+ pi / 2)
+  -- -- Set the mob's position to a random location.
+  get_position mobSpawnLoc >>= Node2D.set_position (upcast @Node2D mob)
+  -- -- Add some randomness to the direction.
+  direction' <- (direction +) <$> randomRIO ((-pi) / 4, pi / 4)
+  set_rotation mob direction'
+  -- -- Set the velocity' (speed & direction).
+  liftM2 (,) (readMVar $ _mMinSpeed mob) (readMVar $ _mMaxSpeed mob)
+    >>= randomRIO
+    >>= (\x -> toLowLevel (V2 x 0))
+    >>= (godot_vector2_rotated `flip` (CFloat direction'))
+    >>= set_linear_velocity mob
+
+
+--- Player
+
 
 data Player = Player { _pBase :: Area2D, _pSpeed :: MVar Float }
 instance HasBaseClass Player where
@@ -109,77 +141,82 @@ instance HasBaseClass Player where
   super = _pBase
 instance NativeScript Player where
   classInit base = Player base <$> newMVar 400
-  classMethods = [method0 "_ready" player_ready
-                 ,method1 "start" player_start
-                 ,method1 "_process" player_process
-                 ,method "_on_Player_body_entered" on_Player_body_entered
-                 ]
-  classProperties = [
-    createMVarProperty "speed" _pSpeed 400
+  classMethods =
+    [ method0 "_ready" player_ready
+    , method1 "_process" player_process
+    , method "_on_Player_body_entered" on_Player_body_entered
+    , method1 "start" player_start
     ]
-  classSignals = [
-    signal "hit" []
-    ]
+  classProperties = [createMVarProperty "speed" _pSpeed (Right 400)]
+  classSignals = [signal "hit" []]
 
 player_ready self = hide self
 
 player_start self pos = do
   set_position self pos
-  G.show self
-  n <- getNode @CollisionShape2D self "CollisionShape2D"
-  set_disabled n False
+  CanvasItem.show self
+  getNode @CollisionShape2D self "CollisionShape2D" >>= (`set_disabled` False)
 
-player_process self deltaSeconds = do
-  rect <- (\(V2 _ r) -> r) <$> (fromLowLevel =<< get_viewport_rect self)
-  Just inp <- getSingleton @Input
-  pr <- is_action_pressed inp =<< toLowLevel "ui_right"
-  pl <- is_action_pressed inp =<< toLowLevel "ui_left"
-  pd <- is_action_pressed inp =<< toLowLevel "ui_down"
-  pu <- is_action_pressed inp =<< toLowLevel "ui_up"
-  let velocity = V2 ((if pl then -1 else 0) + (if pr then 1 else 0))
-                    ((if pu then -1 else 0) + (if pd then 1 else 0))
-  if norm velocity > 0 then
-    do
+player_process self delta = do
+  animSprite              <- getNode @AnimatedSprite self "AnimatedSprite"
+  screenSize              <- get_viewport_rect self >>= fromLowLevel <&> (^. _y)
+  [left, right, up, down] <- do
+    Just inp <- getSingleton @Input
+    let isActionPressed = is_action_pressed inp <=< toLowLevel
+    mapM isActionPressed ["ui_left", "ui_right", "ui_up", "ui_down"]
+  let velocity =
+        let bVal b v = if b then v else 0
+        in  V2 (bVal left (-1) + bVal right 1) (bVal up (-1) + bVal down 1)
+  if velocity /= zero
+    then do
       speed <- readMVar (_pSpeed self)
-      let v = normalize velocity ^* speed
-      get_position self >>= liftLowLevel (\x -> clamp2 zero rect ((v ^* deltaSeconds) ^+^ x)) >>= set_position self
-      n <- getNode @AnimatedSprite self "AnimatedSprite"
-      if v^._x /= 0 then
-        do
-          set_animation n =<< toLowLevel "right"
-          set_flip_h n (v^._x < 0)
-          set_flip_v n False
-        else if v^._y /= 0 then
-               do
-                 set_animation n =<< toLowLevel "up"
-                 set_flip_v n (v^._y > 0)
-             else
-               pure ()
-      play n =<< toLowLevel ""
-    else
-    do
-      n <- getNode @AnimatedSprite self "AnimatedSprite"
-      G.stop n
+      pos   <- get_position self >>= fromLowLevel
+      let velocity' = normalize velocity ^* speed
+          pos'      = pos + velocity' ^* delta
+          clamp v a b = max a (min b v)
+          newPos =
+            let posX = clamp (pos' ^. _x) 0 (screenSize ^. _x)
+                posY = clamp (pos' ^. _y) 0 (screenSize ^. _y)
+            in  V2 posX posY
+      set_position self =<< toLowLevel newPos
+      play animSprite =<< toLowLevel ""
+
+      if velocity' ^. _x /= 0
+        then do
+          set_animation animSprite =<< toLowLevel "right"
+          set_flip_v animSprite False
+          set_flip_h animSprite (velocity' ^. _x < 0)
+        else when (velocity' ^. _y /= 0) $ do
+          set_animation animSprite =<< toLowLevel "up"
+          set_flip_v animSprite (velocity' ^. _y > 0)
+    else AnimatedSprite.stop animSprite
 
 on_Player_body_entered self [body] = do
-  hide self
-  h <- toLowLevel "hit"
-  _ <- emit_signal self h []
-  n <- getNode @CollisionShape2D self "CollisionShape2D"
-  s <- toLowLevel "set_disabled"
-  call_deferred n s [VariantBool True]
+  hide self -- Player disappears after being hit.
+  _ <- (emit_signal self) `flip` [] =<< toLowLevel "hit"
+  getNode @CollisionShape2D self "CollisionShape2D" >>= \cSh -> do
+    disable <- toLowLevel "set_disabled"
+    call_deferred cSh disable [VariantBool True]
 
-data Mob = Mob { _mBase :: RigidBody2D, _mMinSpeed :: MVar Float, _mMaxSpeed :: MVar Float }
+data Mob = Mob
+  { _mBase :: RigidBody2D
+  , _mMinSpeed :: MVar Float
+  , _mMaxSpeed :: MVar Float
+  , _mMobTypes :: MVar [Text]
+  }
 instance HasBaseClass Mob where
   type BaseClass Mob = RigidBody2D
   super = _mBase
 instance NativeScript Mob where
-  classInit base = Mob base <$> newMVar 150 <*> newMVar 250
-  classMethods = [
-    method0 "_ready" $ \self -> do
-        n <- getNode @AnimatedSprite self "AnimatedSprite"
-        name <- randomElement ["walk", "swim", "fly"]
-        set_animation n =<< toLowLevel name
+  classInit base = Mob base
+    <$> newMVar 150
+    <*> newMVar 250
+    <*> newMVar ["walk", "swim", "fly"]
+  classMethods =
+    [ method0 "_ready" $ \self -> do
+        let randElem xs = (xs !!) <$> randomRIO (0, length xs - 1)
+        randAnim <- readMVar (_mMobTypes self) >>= randElem >>= toLowLevel
+        getNode @AnimatedSprite self "AnimatedSprite" >>= (`set_animation` randAnim)
     , method0 "_on_VisibilityNotifier2D_screen_exited" $ \self -> do
         queue_free self
     ]
@@ -190,44 +227,45 @@ instance HasBaseClass HUD where
   super = _hBase
 instance NativeScript HUD where
   classInit base = pure $ HUD base
-  classMethods = [method1 "show_message" show_message
-                 ,method0 "show_game_over" show_game_over
-                 ,method1 "update_score" update_score
-                 ,method0 "_on_StartButton_pressed" _on_StartButton_pressed
-                 ,method0 "_on_MessageTimer_timeout" _on_MessageTimer_timeout
-                 ]
-  classSignals = [
-    signal "start_game" []
+  classMethods =
+    [ method1 "show_message" show_message
+    , method0 "show_game_over" show_game_over
+    , method1 "update_score" update_score
+    , method0 "_on_StartButton_pressed" _on_StartButton_pressed
+    , method0 "_on_MessageTimer_timeout" _on_MessageTimer_timeout
     ]
+  classSignals = [signal "start_game" []]
 
 show_message :: HUD -> GodotString -> IO ()
 show_message self text = do
   messageLabel <- getNode @Label self "MessageLabel"
   set_text messageLabel text
-  G.show messageLabel
-  join $ GT.start <$> (getNode @Timer self "MessageTimer") <*> pure (-1)
+  CanvasItem.show messageLabel
+  getNode @Timer self "MessageTimer" >>= (`Timer.start` (-1))
 
+show_game_over :: HUD -> IO Int
 show_game_over self = do
   show_message self =<< toLowLevel "Game Over"
   timer <- getNode @Timer self "MessageTimer"
-  await self timer "timeout"
-    (\self -> do
-        messageLabel <- getNode @Label self "MessageLabel"
-        set_text messageLabel =<< toLowLevel "Dodge the\nCreeps!"
-        G.show messageLabel
-        G.show =<< getNode @Button self "StartButton") 
+  await self timer "timeout" $ \self -> do
+    messageLabel <- getNode @Label self "MessageLabel"
+    set_text messageLabel =<< toLowLevel "Dodge the\nCreeps!"
+    CanvasItem.show messageLabel
+    getNode @Button self "StartButton" >>= CanvasItem.show
 
+update_score :: HUD -> Int -> IO ()
 update_score self score = do
   l <- getNode @Label self "ScoreLabel"
-  set_text l =<< toLowLevel (T.pack $ Prelude.show (score :: Int))
+  set_text l =<< toLowLevel (T.pack $ Prelude.show score)
 
+_on_StartButton_pressed :: HUD -> IO GodotVariant
 _on_StartButton_pressed self = do
-  G.hide =<< getNode @Button self "StartButton"
-  sg <- toLowLevel "start_game"
-  emit_signal self sg []
+  getNode @Button self "StartButton" >>= CanvasItem.hide
+  (emit_signal self) `flip` [] =<< toLowLevel "start_game"
 
+_on_MessageTimer_timeout :: HUD -> IO ()
 _on_MessageTimer_timeout self = do
-  G.hide =<< getNode @Label self "MessageLabel"
+  getNode @Label self "MessageLabel" >>= CanvasItem.hide
 
 deriveBase ''Main
 deriveBase ''Player
